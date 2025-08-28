fb2cd8eefc615fcd25358df310e48248d9d99fd2
Rafael <camaracruzrafael@gmail.com>
2025-08-28 19:31:13 -0400

    Update script.js 3.3

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 const produtos = [
   { id: 1, nome: "Pastel", preco: 5.00, categoria: "salgado" },
   { id: 2, nome: "Mini pizza", preco: 5.00, categoria: "salgado" },
-  { id: 3, nome: "Refrigerante Lata", preco: 2.00, categoria: "bebida" },
+  { id: 3, nome: "Refrigerante", preco: 2.00, categoria: "bebida" },
   { id: 4, nome: "Espetinho", preco: 5.00, categoria: "salgado" },
   { id: 5, nome: "Alimentação", preco: 10.00, categoria: "salgado" },
   { id: 6, nome: "Salgado", preco: 5.00, categoria: "salgado" }
@@ -386,6 +386,7 @@ window.onload = () => {
 // Executa quando a página carrega e após 1 segundo (dupla verificação)
 document.addEventListener('DOMContentLoaded', corrigirInputCliente);
 setTimeout(corrigirInputCliente, 1000);
+
 
 
 
